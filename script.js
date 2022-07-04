7916842175243580f80a121456b314792f928b7a
Miłosz Grocholewski <65454590+Miligro@users.noreply.github.com>
2022-07-04 10:19:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 const saveBtn = document.getElementById('save_button')
 const myForm = document.forms['my_form']
-const firstName = myForm.elements["first_name"];
-const lastName = myForm.elements["last_name"];
-const email = myForm.elements["email"];
-const description = myForm.elements["description"];
-const gender = myForm.elements["gender"];
+const firstName = myForm.elements.first_name;
+const lastName = myForm.elements.last_name;
+const email = myForm.elements.email;
+const description = myForm.elements.description;
+const gender = myForm.elements.gender;
 
 saveBtn.addEventListener("click", (event)=>{
-    event.preventDefault();
-    const el = document.getElementById("data");
-    el.innerHTML = "";
+    const elem = document.getElementById("data");
+    if(elem){
+        elem.innerHTML = "";
+    }
     if(checkForm()){
-        el.innerHTML = `<p>Imię: ${firstName.value}</p>
-        <p>Nazwisko: ${lastName.value}</p>
-        <p>Email: ${email.value}</p>
-        <p>Opis: ${description.value}</p>
-        <p>Płeć: ${gender.value === "man" ? "Mężczyzna" : "Kobieta"}</p>`;
+        const el = document.createElement("div");
+        el.setAttribute('id', 'data');
+        
+        const firstNameEl = document.createElement("p")
+        firstNameEl.innerText = `Imię: ${firstName.value}`;
+        const lastNameEl = document.createElement("p")
+        lastNameEl.innerText = `Nazwisko: ${lastName.value}`;
+        const emailEl = document.createElement("p")
+        emailEl.innerText = `Email: ${email.value}`;
+        const descriptionEl = document.createElement("p")
+        descriptionEl.innerText = `Opis: ${description.value}`
+        const genderEl = document.createElement("p");
+        genderEl.innerText = `Płeć: ${gender.value === "man" ? "Mężczyzna" : "Kobieta"}`
+        el.appendChild(firstNameEl);
+        el.appendChild(lastNameEl);
+        el.appendChild(emailEl);
+        el.appendChild(descriptionEl);
+        el.appendChild(genderEl);
         document.body.appendChild(el);
         myForm.reset();
     }else{
@@ -25,11 +39,9 @@ saveBtn.addEventListener("click", (event)=>{
 })
 
 function checkForm(){
-    let emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    let nameRegex = /^[a-zA-Z]+$/;
-    const emailRegExp = new RegExp(emailRegex);
-    const nameRegExp = new RegExp(nameRegex);
-    if(firstName.value.trim() === "" || lastName.value.trim() === "" || description.value.trim() === "" || !emailRegExp.test(email.value) || !nameRegExp.test(firstName.value) || !nameRegExp.test(lastName.value)){ 
+    const emailRegExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    const nameRegExp = /^[a-zA-Z]+$/;
+    if(!firstName.value.trim() || !lastName.value.trim() || !description.value.trim() || !emailRegExp.test(email.value) || !nameRegExp.test(firstName.value) || !nameRegExp.test(lastName.value)){ 
         return false;
     };
     return true;
