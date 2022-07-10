273ee888c03d1c7416db2a37dd0a8282210f8b6d
Miligro <milo20054@gmail.com>
2022-07-10 23:02:15 +0200

    Add slider repeat

--- pages/albums/albumPhotos/albumPhotos.js
+++ pages/albums/albumPhotos/albumPhotos.js
@@ -1,60 +1,76 @@
-import {get} from '../../../api.js'
+import { get } from "../../../api.js";
 
-const slideLeftBtn = document.getElementById('slide-left-btn');
-const slideRightBtn = document.getElementById('slide-right-btn');
+const slideLeftBtn = document.getElementById("slide-left-btn");
+const slideRightBtn = document.getElementById("slide-right-btn");
 
 let photos = [];
-let photosEl = [];
 let length = 0;
-let path = window.location.pathname.split('/')
-let photoIndex = 0;
-let sliderWidth = 0;
-async function getPhotos(id){
-    photos = await get(`https://jsonplaceholder.typicode.com/albums/${id}/photos`);
-    showPhotos();
+let path = window.location.pathname.split("/");
+const sliderWidth = 500;
+const sliderHeight = 500;
+
+async function getPhotos(id) {
+  photos = await get(
+    `https://jsonplaceholder.typicode.com/albums/${id}/photos`
+  );
+  showPhotos();
 }
 
-function showPhotos(){
-    const slideGroup = document.getElementById('slider');
-    sliderWidth = slideGroup.offsetWidth;
-    sliderHeight = slideGroup.offsetWidth;
-    for(let photo of photos){
-        const slideEl = document.createElement('div');
-        slideEl.setAttribute('class', 'slide');
+function showPhotos() {
+  const slider = document.getElementById("slider");
+  slider.style.height = `${sliderHeight}px`;
+  slider.style.width = `${sliderWidth}px`;
+  const slides = document.getElementById("slides");
+  for (let photo of photos) {
+    const slideEl = document.createElement("div");
+    slideEl.setAttribute("class", "slide");
 
-        const imgEl = document.createElement('img')
-        imgEl.setAttribute('src', photo.url);
-        imgEl.setAttribute('title',photo.title);
-        imgEl.style.width = sliderWidth + 'px';
-        imgEl.style.height = sliderHeight + 'px';
-        slideEl.appendChild(imgEl)
-        photosEl.push(slideEl);
-        slideGroup.appendChild(slideEl);
-    }
-    length = photosEl.length
+    const imgEl = document.createElement("img");
+    imgEl.setAttribute("src", photo.url);
+    imgEl.setAttribute("title", photo.title);
+    imgEl.style.maxWidth = sliderWidth + "px";
+    imgEl.style.maxHeight = sliderHeight + "px";
+    slideEl.appendChild(imgEl);
+    slides.appendChild(slideEl);
+  }
+  length = photos.length;
+  slides.style.width = `${length * sliderWidth}px`;
+
+  if (length > 1) {
+    slides.style.marginLeft = `-${sliderWidth}px`;
+  }
+  slides.insertBefore(slides.lastElementChild, slides.firstElementChild);
 }
 
-function slide(index){
-    photosEl.forEach((el)=>{
-        el.style.transform = `translateX(-${index*sliderWidth}px)`
-    })
+function slideLeft() {
+  const slides = document.getElementById("slides");
+  slides.style.transition = `left 0.2s`;
+  slides.style.left = `${sliderWidth}px`;
+
+  setTimeout(() => {
+    slides.insertBefore(slides.lastElementChild, slides.firstElementChild);
+    slides.style.transition = ``;
+    slides.style.left = `0px`;
+  }, 200);
 }
 
-slideLeftBtn.addEventListener('click', ()=>{
-    const index = photoIndex - 1
-    if(index >= 0){
-        photoIndex = index;
-        slide(photoIndex);
-    }
-    
-})
+function slideRight() {
+  const slides = document.getElementById("slides");
+  slides.style.transition = `left 0.2s`;
+  slides.style.left = `${-sliderWidth}px`;
+  setTimeout(() => {
+    slides.appendChild(slides.firstElementChild);
+    slides.style.transition = ``;
+    slides.style.left = ``;
+  }, 200);
+}
 
-slideRightBtn.addEventListener('click', ()=>{
-    const index = photoIndex + 1
-    if(index < length){
-        photoIndex = index;
-        slide(photoIndex);
-    }
-})
+slideLeftBtn.addEventListener("click", () => {
+  slideLeft();
+});
 
-getPhotos(+path[path.length-1])
+slideRightBtn.addEventListener("click", () => {
+  slideRight();
+});
+
+getPhotos(+path[path.length - 1]);
